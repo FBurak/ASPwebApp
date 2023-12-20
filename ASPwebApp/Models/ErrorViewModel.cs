b0f72825d4bdd8ec0c4f2ea506c0e2e697403e10
FBurak <nbfidan5838@gmail.com>
2023-12-20 16:28:39 +0300

    Add project files.

---
+++ ASPwebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPwebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASPwebApp/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPwebApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(16)]
+        public string Password { get; set; }
+
+    }
+}
